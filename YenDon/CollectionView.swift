6a285b5998179b67c975b34e79e396bab98231ec
kyokoshima <dev.kyoko@gmail.com>
2017-05-14 19:12:43 +0700

    replace old repository

---
+++ YenDon/CollectionView.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionView.swift
+//  YenDon
+//
+//  Created by 横島健一 on 2017/05/13.
+//  Copyright © 2017年 info.tmpla. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var labelRate: UILabel!
+    @IBOutlet weak var ind: UIActivityIndicatorView!
+    @IBOutlet weak var textAmount: UITextField!
+    @IBOutlet weak var image: UIImageView!
+}
--- YenDon/ViewController.swift
+++ YenDon/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:CollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        let image:UIImageView = cell.image
+        image.image = [#imageLiteral(resourceName: "Japan"), #imageLiteral(resourceName: "United-States"), #imageLiteral(resourceName: "Australia")][indexPath.row]
+        cell.tag = indexPath.row
+        return cell
+    }
+    
+    // FlowLayoutの
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize: CGFloat = self.view.frame.size.width
+        return CGSize(width: cellSize, height: collectionView.frame.size.height)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0,0,0,0)
+    }
 }
 
