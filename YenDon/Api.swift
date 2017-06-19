06fce8fa0d51cd2ac4786d03005229d6d44a7480
kyokoshima <dev.kyoko@gmail.com>
2017-06-19 21:37:55 +0700

    try to add test

--- YenDon/Api.swift
+++ YenDon/Api.swift
@@ -6,30 +6,36 @@
 //  Copyright © 2017年 info.tmpla. All rights reserved.
 //
 
+import UIKit
 import Alamofire
 
-private let host = "https://example.com"
+private let host = "https://query.yahooapis.com/v1/public/yql"
 // https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDVND,VNDJPY,AUDVND%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22EURUSD%22%2C%22GBPUSD%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
 struct Api {
-    let url:String
-    let method: HTTPMethod
+    let url:String = host
+    let method: HTTPMethod = .get
     let parameters: Parameters
     
-    init(path: String, method: HTTPMethod = .get, parameters: Parameters = [:]) {
-        url = host + path
-        self.method = method
-        self.parameters = parameters
-    
+    init( pairs: Array<String> = ["JPYVND"]) {
+        let pairsWithValue = pairs.map {
+            return "\"\($0)\""
+            }.joined(separator: ",")
+        self.parameters = [ "format": "json",
+                            "store": "store://datatables.org/alltableswithkeys",
+                            "q": "select * from yahoo.finance.xchange where pair in \(pairsWithValue)"
+        ]
+        
     }
     
     func request(success: @escaping(_ data: Dictionary<String, Any>) ->Void, fail: @escaping(_ error: Error?) -> Void) {
         Alamofire.request(url, method: method, parameters: parameters).responseJSON { response in
-            if response.result.ifSuccess {
+            if response.result.isSuccess {
                 success(response.result.value as! Dictionary)
             } else {
                 fail(response.result.error)
             }
-        
+            
         }
     }
 }
--- YenDonTests/YenDonTests.swift
+++ YenDonTests/YenDonTests.swift
@@ -32,5 +32,7 @@ class YenDonTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
+    func testApi() {
+        
+    }
 }
